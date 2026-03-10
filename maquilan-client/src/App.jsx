7d882f22d85c1ffa9eaf6c635f9ce21ecb3fd9b7
unknown <joshuamaquilan31@gmail.com>
2026-03-10 13:16:59 +0800

    lab-act1

--- maquilan-client/src/App.jsx
+++ maquilan-client/src/App.jsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My React App!</h1>
+          <p>
+            Name: Joshua Maquilan<br />
+            Email: joshuamaquilan31@gmail.com<br />
+            Other info: <a href="https://github.com/Metalxr/maquilan-webprog">My GitHub Repository</a>
+          </p>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
